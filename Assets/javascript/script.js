69ab3f0e2b7b53db864ecbaf91a47e19643936de
Sam Fox <samueljasonfox@gmail.com>
2020-12-21 13:59:55 -0800

    organize files

---
+++ Assets/javascript/script.js
@@ -0,0 +1,68 @@
+console.log('linked!')
+
+// var key = "20bad3c2c2a34e2a9ada0c04f778f495" 
+// var region = "eastus"
+
+// status fields and start button in UI
+var phraseDiv;
+var startRecognizeOnceAsyncButton;
+
+// subscription key and region for speech services.
+var subscriptionKey, serviceRegion;
+var authorizationToken;
+var SpeechSDK;
+var recognizer;
+
+document.addEventListener("DOMContentLoaded", function () {
+  startRecognizeOnceAsyncButton = document.getElementById("startRecognizeOnceAsyncButton");
+  subscriptionKey = document.getElementById("subscriptionKey");
+  serviceRegion = document.getElementById("serviceRegion");
+  phraseDiv = document.getElementById("phraseDiv");
+
+  startRecognizeOnceAsyncButton.addEventListener("click", function () {
+    startRecognizeOnceAsyncButton.disabled = true;
+    phraseDiv.innerHTML = "";
+
+    // if we got an authorization token, use the token. Otherwise use the provided subscription key
+    var speechConfig;
+    if (authorizationToken) {
+      speechConfig = SpeechSDK.SpeechConfig.fromAuthorizationToken(authorizationToken, serviceRegion.value);
+    } else {
+      if (subscriptionKey.value === "" || subscriptionKey.value === "subscription") {
+        alert("Please enter your Microsoft Cognitive Services Speech subscription key!");
+        return;
+      }
+      speechConfig = SpeechSDK.SpeechConfig.fromSubscription(subscriptionKey.value, serviceRegion.value);
+    }
+
+    speechConfig.speechRecognitionLanguage = "en-US";
+    var audioConfig = SpeechSDK.AudioConfig.fromDefaultMicrophoneInput();
+    recognizer = new SpeechSDK.SpeechRecognizer(speechConfig, audioConfig);
+
+    recognizer.recognizeOnceAsync(
+      function (result) {
+        startRecognizeOnceAsyncButton.disabled = false;
+        phraseDiv.innerHTML += result.text;
+        window.console.log(result);
+
+        recognizer.close();
+        recognizer = undefined;
+      },
+      function (err) {
+        startRecognizeOnceAsyncButton.disabled = false;
+        phraseDiv.innerHTML += err;
+        window.console.log(err);
+
+        recognizer.close();
+        recognizer = undefined;
+      });
+  });
+
+  if (!!window.SpeechSDK) {
+    SpeechSDK = window.SpeechSDK;
+    startRecognizeOnceAsyncButton.disabled = false;
+
+    document.getElementById('content').style.display = 'block';
+    document.getElementById('warning').style.display = 'none';
+  }
+});
