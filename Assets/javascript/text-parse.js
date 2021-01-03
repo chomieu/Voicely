8ada380974e3a902f43e4c34b05a7390d75f7a3a
Tim Keller <timothy.m.keller@gmail.com>
2021-01-02 18:06:02 -0800

    Negated Microsoft's decision to put :) smiley faces into their speech SDK's return value whenever someone says 'smiley face'. Modified the main RegEx to include a possible period it can match to, allowing it to stop ending sentences that end with emojis or symbols with periods. The array holding all the emojis and characters is now called emojiObjects, the s having been added to it from the singular form. Clarified some comments. Added the matchFound variable so an undefined entry will be pushed to the emojiArray if no emoji is found. This fixes a bug I found earlier related to multiple calls to Voicely in one sentence where an earlier one fails, leading to an emoji not being placed properly. Removed periods from the regex that modifies newText.

--- Assets/javascript/text-parse.js
+++ Assets/javascript/text-parse.js
@@ -1,26 +1,33 @@
 // https://emoji-api.com/
 
 // Takes in a string, matches a regex with any phrases in it, then returns emojis created from those matches to the #emoji-display div.
-function voiceSearch( matchText, emojiObject ) {
+function voiceSearch( matchText, emojiObjects ) {
+    // Undo Microsoft's questionable decision to replace "smiley face" with ":)".
+    console.log( "Smiley face test match? ", /:\)/.test( matchText ) );
+    if ( /:\)/.test( matchText ) ) {
+        console.log( "Entered if statement for smiley match" );
+        matchText = matchText.replace( /:\)/g, "slightly smiling face" );
+    }
     // construct our RegEx of the keyphrase.
     // var regEx = /(S|s)ymbol[s]?[,]? (E\d0 )?[\w+( |,|\.|\?|!)]+?(E|e)moji/g;
-    var regEx = /(V|v)oice[,]? (L|l)(ee||i)[s]?[,]? (E\d0 )?[\w+( |,|\.|\?|!)]+?((E|e)moji|(S|s)ymbol)/g;
+    var regEx = /(V|v)oice[,]? (L|l)(ee||i)[s]?[,]? (E\d0 )?[\w+( |,|\.|\?|!)]+?((E|e)moji|(S|s)ymbol)\.?/g;
     console.log( "1. ******** NEW CALL OF voiceSearch ********");
     console.log( "2. Initial RegEx: ", regEx );
     console.log( "3. Text to match: ", matchText );
+    // Undo Microsoft's questionable decision to replace the word "smiley face" with 
     var matchArray = matchText.match( regEx );
     var emojiArray = [];
     console.log( "Array of matched phrases: ", matchArray );
     // Gets the current contents of the div, in case there was already content there before running voiceSearch.
     var oldText = $( "#phraseDiv" ).val();
     console.log( "oldText:", oldText );
-    // If the RegEx has at least one match AND an emojiObject has been passed in (the latter shouldn't be a problem anymore since Chomie added the cached emoji.json file, but it's a nice failsafe in case we use this function in other contexts).
-    if ( matchArray && emojiObject ) {
+    // If the RegEx has at least one match AND an emojiObjects has been passed in (the latter shouldn't be a problem anymore since Chomie added the cached emoji.json file, but it's a nice failsafe in case we use this function in other contexts).
+    if ( matchArray && emojiObjects ) {
         console.log( "4. Entered matchArray if statement" );
         // Iterate over matchArray and build an array of keywords to replace.
         for ( var i = 0; i < matchArray.length; i++ ) {
             /* This complicated line of code preps the matched string to be converted into an emoji or symbol slug. It takes the matched string and does the following: */
-            // - The substring() method call removes the first speech tag by removing everything BEFORE the SECOND instance of whitespace, and removing everything AFTER the LAST instance of whitespace.
+            // - The substring() method call removes the "speech tags" by removing everything BEFORE the SECOND instance of whitespace, and removing everything AFTER the LAST instance of whitespace.
             // - The trim() method removes all whitespace from either end of the string resulting from the substring() method. 
             // - The replace() method removes all commas.
             // - toLowerCase() converts everything to lowercase letters.
@@ -32,26 +39,35 @@ function voiceSearch( matchText, emojiObject ) {
                 the proper format for an emoji slug. */
                 strippedString = strippedString.slice( 0, 2) + " " + strippedString.slice( 2, strippedString.length );
             };
-            console.log( "6. Matched string stripped of symbol words and commas: ", strippedString );
+            console.log( "6. strippedString, with spaces inserted for emoji variant tag: ", strippedString );
+            // Replaces all spaces with dashes to create a string that corresponds with the "slug" key for emojis in the emojiObjects array.
             var dashedString = strippedString.replace( /\s/g, "-" );
             console.log( "7. Matched string with dashes instead of spaces: ", dashedString );
-            // Iterates over entire emojiObject looking for slugs that match the "dashed" version of what was said.
-            for ( var j = 0; j < emojiObject.length; j++ ) {
-                if ( "variants" in emojiObject[ j ] ) {
-                    var variantObj = emojiObject[ j ].variants;
+            // Iterates over entire emojiObjects array looking for slugs that match the "dashed" version of what was said.
+            var matchFound = false;
+            for ( var j = 0; j < emojiObjects.length; j++ ) {
+                // The following if statement first searches the "variant" property in emoji objects stored in
+                // emojiObjects array.
+                if ( "variants" in emojiObjects[ j ] ) {
+                    var variantObj = emojiObjects[ j ].variants;
                     for ( var k = 0; k < variantObj.length; k++ ) {
                         if ( variantObj[ k ].slug == dashedString ) {
                             emojiArray.push( variantObj[ k ].character );
-                        };
+                            matchFound = true;
+                        }
                     };
                 };
-                if ( emojiObject[ j ].slug == dashedString ) {
-                    emojiArray.push( emojiObject[ j ].character );
-                };
+                if ( emojiObjects[ j ].slug == dashedString ) {
+                    emojiArray.push( emojiObjects[ j ].character );
+                    matchFound = true;
+                }
             };
+            if ( !matchFound ) {
+                emojiArray.push( undefined );
+            }
         };
         console.log( "8. Array of emojis: ", emojiArray );
-        // Iterate over matchArray, and replace all instances of matchArray[ i ] in matchText with emojiArray[ i ].
+        // Iterate over matchArray, and replace all instances of matchArray[ i ] in matchText with emojiArray[ i ], where they exist.
         var newText = matchText;
         for ( var i = 0; i < matchArray.length; i++ ) {
             var regEx2 = new RegExp( matchArray[ i ] );
@@ -64,8 +80,9 @@ function voiceSearch( matchText, emojiObject ) {
             }
         }
         // Remove punctuation from new line speech tags that have them immediately afterward.
-        newText = newText.replace( /\n(\.|\?|!|,)(\s)?/, "\n" );
-        newText = newText.replace( /- (\.|\?|!|,)(\s)?/, "- ");
+        // Periods (.) are already accounted for in the RegEx.
+        newText = newText.replace( /\n(\?|!|,)(\s)?/, "\n" );
+        newText = newText.replace( /- (\?|!|,)(\s)?/, "- ");
         // Place the result text in the #phraseDiv
         console.log( "11. Text to be placed in #phraseDiv: ", newText );
         $( "#phraseDiv" ).val( `${ oldText } ${ newText }`);
